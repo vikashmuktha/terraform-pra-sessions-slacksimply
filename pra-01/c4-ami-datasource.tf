dcc67cf5ba1ca2fa18ac0d02ddad8fefe477a417
vikash <mukthavikash@gmail.com>
2023-04-03 19:11:33 +0530

    Ami Datasource

---
+++ pra-01/c4-ami-datasource.tf
@@ -0,0 +1,22 @@
+# Get latest AMI ID for Amazon Linux2 OS
+data "aws_ami" "amzlinux2" {
+  most_recent      = true
+  owners           = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-gp2"]
+    // Amazon Linux 2 Kernel 5.10 AMI 2.0.20230320.0 x86_64 HVM gp2
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
